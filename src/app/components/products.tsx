d6f58979faf3004874dd721f31371ed2b76f7415
Fiachra Ward <47219191+fward01@users.noreply.github.com>
2024-11-07 12:45:40 +0000

    Updated to work, no errors, add and delete functional

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -15,23 +15,30 @@ export default function Products() {
       fetch('products.json')
         .then(response => response.json())
         .then(json => setData(json));
-    });
+    },[]);
   
     if (!data) {
       return <p>Loading...</p>;
     }
-  
-
+    const deleteLastitem = ()=>{const datacopy = [...data];
+      datacopy.pop();
+      setData(datacopy)};
+    const addSpeghetti=() =>{console.log(data); 
+      setData([...data,{id:"5", name: "Spaghetti"}]);};
     return (
-        <div>
+    <div>
       <ul>
         {(data as Item[]).map(item => (
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
-      <button onClick={()=> {const newdata =  data.concat({id:"5", name: "Spaghetti"}); setData(newdata)}} value="Spaghetti">
-      Add Spaghetti
-    </button>
+      <button onClick={addSpeghetti} value="Spaghetti">
+         Add Spaghetti
+      </button>
+      <button onClick={deleteLastitem} value="NoSpaghetti">
+        Delete last item 
+      </button>
+    <p></p>
     </div>      
     );
   }
